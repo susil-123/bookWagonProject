b70b4f62bbb5d536cab10a9c4013fa3cf0dd6792
Susil Kumar K <susilkumarkct@gmail.com>
2025-05-27 01:35:23 +0530

    Update PersonalSettingsStepDefinition.java

--- src/test/java/com/booksWagon/stepDefinition/PersonalSettingsStepDefinition.java
+++ src/test/java/com/booksWagon/stepDefinition/PersonalSettingsStepDefinition.java
@@ -8,6 +8,7 @@ import org.testng.Assert;
 
 import com.booksWagon.pages.PersonalSettings;
 
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.BeforeStep;
 import io.cucumber.java.en.And;
@@ -28,16 +29,10 @@ public class PersonalSettingsStepDefinition {
 	    else obj.dummyLogin();
 	    
 	    obj.gotoHomePage();
-	    
-	    
-	    String expectedURL = "https://www.bookswagon.com/";
-	    String actualURL = obj.getCurrentURL();
-	    Assert.assertEquals(actualURL,expectedURL);
 	}
 
 	@When("I navigate to personal settings")
 	public void i_navigate_to_personal_settings() {
-	    // Write code here that turns the phrase above into concrete actions
 		System.out.println("i_navigate_to_personal_settings");
 		
 		if(obj.checkLogin())System.out.println("logout found");
@@ -46,36 +41,13 @@ public class PersonalSettingsStepDefinition {
 		obj.gotoPersonalSettings();
 	}
 
-	@And("I fill valid {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string}, {string} credentials and update")
-	public void i_fill_valid_credentials(String firstName, String lastName, String email, String fax, String profileName,
-			String wishList, String newsLetter, String transMail, String promoMail, String countryCode, String mobile) throws IOException {
-	    // Write code here that turns the phrase above into concrete actions
-		System.out.println("i_fill_valid_credentials");
-		obj.fillDetailsTables( firstName,  lastName,  email,  fax,  profileName,  wishList,  newsLetter,  transMail,  promoMail,  countryCode,  mobile);
-		
-	}
-
 	@And("I fill credentials and update")
 	public void i_fill_credentials_and_update() throws IOException, InterruptedException {
-//		ArrayList<String>expectedTestCase = new ArrayList(Arrays.asList("pass","fail","pass"));
-//		ArrayList<String>actualTestCase = obj.fillDetailsExcel();
 		obj.fillDetailsExcel();
-//		ArrayList<String>expectedTestCase = obj.returnExpectedTestCases();
-//		for(int i=0;i<expectedTestCase.size();i++) {
-//			String expected = expectedTestCase.get(i);
-//			String actual = actualTestCase.get(i);
-//			System.out.println("expected: "+expected+" "+"actual: "+actual);
-//			Assert.assertEquals(actual, expected);
-//		}
 	}
 	
 	@Then("the details should be updated")
 	public void the_details_should_be_updated() throws IOException {
-	    // Write code here that turns the phrase above into concrete actions
-//		System.out.println("the_details_should_be_updated");
-//		String expectedURL = "https://www.bookswagon.com/myaccount.aspx";
-//	    String actualURL = obj.getCurrentURL();
-//	    Assert.assertEquals(actualURL,expectedURL);
 		ArrayList<String>expectedTestCase = obj.returnColumn(11);
 		ArrayList<String>actualTestCase = obj.returnColumn(12);
 		for(int i=0;i<expectedTestCase.size();i++) {
@@ -88,7 +60,11 @@ public class PersonalSettingsStepDefinition {
 
 	@Before
 	public void homePage() {
-		System.out.println("before tag");
+		obj.gotoHomePage();
+	}
+	
+	@After
+	public void reset() {
 		obj.gotoHomePage();
 	}
 	
